58933f2d7a6aee8361947c1d2b40d62c4da72949
Chocorrolito13 <118304022+Chocorrolito13@users.noreply.github.com>
2022-11-30 21:31:43 -0600

    Update GitHub.java

--- src/main/java/com/mycompany/github/GitHub.java
+++ src/main/java/com/mycompany/github/GitHub.java
@@ -2,19 +2,5 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
-package com.mycompany.github;
-
-/**
- *
- * @author
- */
-public class GitHub {
-
-    public static void main(String[] args) {
-        System.out.println("No se que acabo de hacer, pero espero que funcione :c");
-        System.out.println("Andres Espinoza estuvo aqui")
-            
-        
     }
 }
