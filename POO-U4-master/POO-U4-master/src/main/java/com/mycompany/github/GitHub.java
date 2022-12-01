238515a625c1d0132c91e85993ebcb3eafc45326
Chocorrolito13 <118304022+Chocorrolito13@users.noreply.github.com>
2022-11-30 19:14:16 -0600

    Add files via upload

---
+++ POO-U4-master/POO-U4-master/src/main/java/com/mycompany/github/GitHub.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.github;
+
+/**
+ *
+ * @author jonat
+ */
+public class GitHub {
+
+    public static void main(String[] args) {
+        System.out.println("No se que acabo de hacer, pero espero que funcione :c");
+        System.out.println("Dominguez Hernandez Jose Francisco, Estuvo aqui :)");
+    }
+}
