dbd812e2976a7931c386ecb8c2cebed0dbc7b5d9
jonathanlic <118302543+jonathanlic@users.noreply.github.com>
2022-11-30 20:50:29 -0700

    Update GitHub.java

--- src/main/java/com/mycompany/github/GitHub.java
+++ src/main/java/com/mycompany/github/GitHub.java
@@ -7,11 +7,13 @@ package com.mycompany.github;
 
 /**
  *
- * @author jotin
+ * @author 
  */
 public class GitHub{
     
     public static void main(String[] args){
+        System.out.println(" Jonathan alexis salas montoya ");
+        System.out.println(" Jonathan eli rodriguez marrufo ");
         
     }
 }
