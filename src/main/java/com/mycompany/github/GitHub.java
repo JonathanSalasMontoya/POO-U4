6b423cc2c8c4a16b81866b669dfb4cd3b4991cd8
jonathanlic <118302543+jonathanlic@users.noreply.github.com>
2022-11-30 20:24:39 -0700

    Update GitHub.java

--- src/main/java/com/mycompany/github/GitHub.java
+++ src/main/java/com/mycompany/github/GitHub.java
@@ -14,6 +14,7 @@ public class GitHub {
     public static void main(String[] args) {
         System.out.println("No se que acabo de hacer, pero espero que funcione :c");
         System.out.println("Andres Espinoza estuvo aqui")
+            //pancho eres mi crush
         
     }
 }
